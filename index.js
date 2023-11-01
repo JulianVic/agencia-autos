5d019a21297575b43b0d0af9a94d05453faaf201
JulianVic <josejulianvicenteperez@gmail.com>
2023-10-31 23:52:32 -0600

    Añadi las rutas de devoluciones

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 import express from 'express'
-import clientesRouter from './src/routes/clientes.js'
 import { conectarDB } from './src/db/db.js'
+import clientesRouter from './src/routes/clientes.js'
 import vehiculosRouter from './src/routes/vehiculo.js'
 import ventasRouter from './src/routes/venta.js'
 import vendedorRoutes from './src/routes/vendedor.js'
+import devolucionesRoutes from './src/routes/devoluciones.js'
 
 const app = express()
 const puerto = process.env.PORT || 4000
@@ -17,6 +18,7 @@ app.use('/api/clientes', clientesRouter)
 app.use('/api/vehiculos', vehiculosRouter)
 app.use('/api/ventas', ventasRouter)
 app.use('/api/vendedor', vendedorRoutes)
+app.use('/api/devoluciones', devolucionesRoutes)
 
 app.listen(puerto, () => {
     console.log('Servidor corriendo en puerto', puerto)
